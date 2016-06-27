21fb442ea29cac35e50d3a224853bcb969ab896c
John Flores <johnroyflores@gmail.com>
2016-06-27 12:04:42 -0500

    added .gitignore file

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'MXJ4QLQ2Ve0exp4UcentwaGqH',
+  consumer_secret: 'VwygHdWa8AXy4ORemLBLX9QG7FitEn1OMEeZ6QunXfq9DdKM37',
+  access_token_key: '540700420-HeOihNJp6n5ncWekbeiZZXaFVTmcc5slxsFhNk9L',
+  access_token_secret: 'p2fJP8YezpIFtnYUlyRSiae4ed6RIV5E2WQsP5iGRZwnW',
+}
